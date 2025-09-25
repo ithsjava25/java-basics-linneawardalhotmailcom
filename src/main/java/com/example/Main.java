4c1dee78edaa6ed9a78f7df292a750a7df36337c
Linnea Vardal <linnea.wardal@hotmail.com>
2025-09-25 13:01:49 +0200

    Uppdaterad kod

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,17 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+        var prices = elpriserAPI.getPriser(LocalDate.now(), ElpriserAPI.Prisklass.SE3);
+        System.out.println(prices.get(0).timeStart());
+
+        /*System.out.println("Tid: " + elpriser.timeStart().toLocalTime() +"-"+ elpriser.timeEnd().toLocalTime()
+                + " Pris: " + formateratPris + " öre/KWh");*/
+
+
     }
 }
